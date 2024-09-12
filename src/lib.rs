04376e0cb1186c0c618bb81a35784231d6d35b5c
wefcdse <3371348169@qq.com>
2024-09-12 15:07:22 +0800

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -385,6 +385,29 @@ fn gen_impl(
 ///
 /// # Example
 ///
+/// ### basic usage
+///
+/// ```rust
+/// use functional_trait::functional_trait;
+///
+/// #[functional_trait]
+/// trait E<'a, T: 'a + ?Sized, const AA: usize, T1>: Sized + Clone + Send
+/// where
+///     T1: Send + Sync,
+///     T: std::fmt::Display,
+/// {
+///     unsafe fn e<'c>(&'c self, a: &'a T, b: [i32; AA], t1: T1) -> &'a str;
+/// }
+///
+/// let fe = |a: &str, b: [i32; 4], _c: i128| {
+///     dbg!(a);
+///     dbg!(b);
+///     "413"
+/// };
+/// unsafe { fe.e("4fr13", [3, 5, 1, 1], 9) };
+///
+/// ```
+///
 ///
 /// ### use as helper trait
 /// ```rust
@@ -408,27 +431,7 @@ fn gen_impl(
 /// }
 /// take_async(async1);
 /// ```
-/// ### basic usage
-/// ```
-/// use functional_trait::functional_trait;
-///
-/// #[functional_trait]
-/// trait E<'a, T: 'a + ?Sized, const AA: usize, T1>: Sized + Clone + Send
-/// where
-///     T1: Send + Sync,
-///     T: std::fmt::Display,
-/// {
-///     unsafe fn e<'c>(&'c self, a: &'a T, b: [i32; AA], t1: T1) -> &'a str;
-/// }
-///
-/// let fe = |a: &str, b: [i32; 4], _c: i128| {
-///     dbg!(a);
-///     dbg!(b);
-///     "413"
-/// };
-/// unsafe { fe.e("4fr13", [3, 5, 1, 1], 9) };
-///
-/// ```
+
 ///
 ///
 #[proc_macro_attribute]
